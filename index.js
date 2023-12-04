f9c601c25d392647757334c1237a0789b53f246b
Abdulla505 <55316736+Abdulla505@users.noreply.github.com>
2023-12-05 02:10:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+ function bein(){
+ window.location.href = "bein.html";
+ }
+
+ 
+ function abs(){
+ window.location.href = "abs.html";
+ }
+ 
+ function ssc(){
+ window.location.href = "ssc.html";
+ }
+ 
+ 
+ function ALKASS(){
+ window.location.href = "ALKASS.html";
+ }
+ 
+ function ontime() {
+ window.location.href = "ontime.html";
+ }
+ 
---
+++ ssc.js
@@ -0,0 +1,34 @@
+function ssc1(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc1";
+ }
+
+ 
+ function ss2(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc2";
+ }
+ 
+ function ssc3(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc3";
+ }
+ 
+ 
+ function ssc4(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc4";
+ }
+ 
+ function ssc5() {
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc_5";
+ }
+ 
+ function sscEX1(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc_extra1";
+ }
+ 
+ 
+ function sscEX2(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc_extra2";
+ }
+ 
+ function sscEX3(){
+ window.location.href = "https://www.ipslow.com/tv/canal.php?id=ssc_extra3";
+ }
